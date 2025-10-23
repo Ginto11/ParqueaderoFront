1f053015289324bcb7a1cbe8364afd30d6e80325
Ginto11 <salinitosnelson@gmail.com>
2025-10-23 17:48:35 -0500

    Revision de error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -64,8 +64,11 @@ export const routes: Routes = [
     },
     {
         path: 'dashboard-admin',
-        loadComponent: () => import('./pages/dashboards/dashboard-admin/dashboard-admin.component'),
         children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/dashboards/dashboard-admin/dashboard-admin.component'),
+            },
             {
                 path: 'gestion-parqueaderos',
                 loadComponent: () => import('./pages/dashboards/dashboard-admin/gestionar-parqueadero/gestionar-parqueadero.component')
